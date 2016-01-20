338c444e31dac98aeb622e6ef492805fded9577e
ichitomo <ichitomo0421@gmail.com>
2016-01-20 14:06:53 +0900

    円運動の記述

--- src/LeapApp.cpp
+++ src/LeapApp.cpp
@@ -140,7 +140,7 @@ public:
         
         // 表示フォントの設定
         mFont = Font( "YuGothic", 20 );
-        
+        mFontColor = ColorA(0.65, 0.83, 0.58);//文字の色
         
         // カメラ(視点)の設定
         mCameraDistance = 1500.0f;//カメラの距離（z座標）
@@ -293,32 +293,28 @@ public:
         mMagSpectrum = mMonitorSpectralNode->getMagSpectrum();
         
         graphUpdate();
+        //周回グラフのアップデート
+        //R = R + sumJes2();
     }
     
     //描写処理
     void draw(){
         gl::clear();
         drawBackgroundColor();
-        
-        gl::setMatrices( mMayaCam.getCamera() );
+        //gl::setMatrices( mMayaCam.getCamera() );
         gl::pushMatrices();
             drawListArea();//メッセージリストの表示
             drawCircle();//サークルで表示
+            drawCircle2();//サークルで表示
+            drawCircle3();//サークルで表示
             //drawPainting();//指の軌跡を描く
             //drawAudioAnalyze();//音声解析の描写
             //drawSinGraph();//sinグラフを描く
             drawBarGraph();
-            drawBox();
             drawAccessNumber();
-        gl::popMatrices();
         
-        //アクセス数に応じてマリオネットを表示
-//        for(int i = 0; i < sumOfFrag(); i++){
-//            gl::pushMatrices();
-//            translate(Vec2d(i*200,0));
-//            drawMarionette();//マリオネット描写
-//            gl::popMatrices();
-//        }
+            //drawAccessNumber();
+        gl::popMatrices();
 
         //アクセス数に応じてマリオネットを表示
         for(int i = 0; i < sumOfFrag(); i++){
@@ -327,8 +323,8 @@ public:
             drawObjFile();
             gl::popMatrices();
         }
-        drawObjFile();
-        mParams.draw();
+        //drawObjFile();
+        //mParams.draw();
     }
     //背景色を変える
     void drawBackgroundColor(){
@@ -417,7 +413,6 @@ public:
         gl::popMatrices();
         
     }
-    
     //マリオネット
     void drawObjFile(){
         
@@ -476,36 +471,149 @@ public:
         gl::popMatrices();
     }
     
-    //メッセージリスト
-    void drawListArea(){
-        //stringstream mm;
-//        gl::pushMatrices();
-//        auto tbox0 = TextBox().alignment( TextBox::LEFT ).font( mFont ).text ( mm.str() ).color(Color( 1.0f, 1.0f, 1.0f )).backgroundColor( ColorA( 0, 1.0f, 0, 0 ) );
-//        auto mTextTexture = gl::Texture( tbox0.render() );
-//        gl::draw( mTextTexture );
-//  
-//        gl::popMatrices();
-//        if(l < 0){
-//            gl::drawString(message, Vec2d(0,0));
-//        }
-    }
-    
-    //サークル（手の数によって大きくなる球体の描写）
+    //ScreenTapの回数によって大きくなる円の描写
     void drawCircle(){
-        //sine, cosineを使った曲線的な拡大縮小///////////////////////////
-        //この場合-A*sin(w*radians(t) - p)の計算結果は100.0~-100.0なので、
-        //100を足すことによって、0~200にしている。
-        int handRadius = sumHands();
-        //y = A*sin(w*(t * PI / 180.0) - p) + 100;
+        //ScreenTapの回数によって大きくなる円の描写
+        int handRadius = sumJes2();
+        int rad = (R + handRadius);
+        setDiffuseColor( ci::ColorA(0.65, 0.83, 0.58));
         gl::pushMatrices();
-        gl::drawSphere(Vec3d( 360, 675, 0 ), handRadius*10);//指の位置
-        gl::drawString(toString(handRadius), Vec2d( 360, 100));
+        gl::drawString("この値はScreenTapの回数によって大きくなる円のhandRadius"+toString(handRadius), Vec2d( 360, 100));
+        gl::drawSolidCircle(Vec2d( 360, WindowHeight/2 ), rad * 8);//ジェスチャーによって円の半径が変わる
         gl::popMatrices();
+        setDiffuseColor( ci::ColorA( 0.8, 0.8, 0.8 ) );
 
     }
+    //Circleジェスチャーによって移動する円の描写
+    void drawCircle2(){
+        //円の周回運動
+        int handSpeed = sumJes1();//サークルジェスチャーの回数
+        int handRadius = sumJes2();//スクリーンタップジェスチャーの回数
+        int rad = (R + handRadius);
+        //float theta = angle * PI /180;  //thetaは角度（angle）をラディアン値に直したもの
+        float theta = handSpeed * PI /180;  //thetaは角度（handSpeed）をラディアン値に直したもの
+        setDiffuseColor( ci::ColorA(0.65, 0.83, 0.58));
+        gl::pushMatrices();
+        gl::drawString("この値はCircleジェスチャーによって移動する円のR"+toString(R), Vec2d( 360, 800));
+        //円を描く
+        //gl::drawStrokedCircle(Vec2d( 360, WindowHeight/2 ), R * 20);//ジェスチャーによって円の半径が変わる//テスト用
+        gl::drawStrokedCircle(Vec2d( 360, WindowHeight/2 ), rad * 10);//ジェスチャーによって円の半径が変わる
+        //円の周りを動く円のアニメーション
+//        posX = R * 20 * cos(theta);//テスト用
+//        posY = -R * 20 * sin(theta);//テスト用
+        posX = rad * 10 * cos(theta);
+        posY = -rad * 10 * sin(theta);
+        gl::drawSolidCircle(Vec2d( posX + 360, posY + WindowHeight/2 ), 10);
+        //angle ++;//テスト用
+        //R = R + 1;//テスト用
+//        if (angle >= 360) angle = 0;  //もしangleが360以上になったら0にする。//テスト用
+        if (handSpeed >= 360) handSpeed = 0;  //もしangleが360以上になったら0にする。
+//        if (R * 10 > 400) R = 10;//テスト用
+        if ((R + handRadius) * 10 > 400) rad = 400;
+        gl::popMatrices();
+        setDiffuseColor( ci::ColorA( 0.8, 0.8, 0.8 ) );
+    }
+    //説明用の円
+    void drawCircle3(){
+    //説明用の円
+        gl::pushMatrices();
+        gl::drawSphere(Vec3d( 360, 675, 0 ), 10);//指の位置
+        gl::popMatrices();
+        setDiffuseColor( ci::ColorA( 0.8, 0.8, 0.8 ) );
+        
+    }
+    
+    //メッセージリスト
+    void drawListArea(){
+        
+        //"大きな声で"描写
+        gl::pushMatrices();
+        gl::drawString(messageList[0],Vec2f(992.5, 145), mFontColor, mFont);
+        gl::translate(Vec2f(992.5, 145));
+        drawBox();
+        gl::popMatrices();
+        
+        //"頑張れ"描写
+        gl::pushMatrices();
+        gl::drawString(messageList[1],Vec2f(992.5, 215), mFontColor, mFont);
+        gl::translate(Vec2f(992.5, 215));
+        drawBox();
+        gl::popMatrices();
+        
+        //"もう一度説明して"描写
+        gl::pushMatrices();
+        gl::drawString(messageList[2],Vec2f(992.5, 285), mFontColor, mFont);
+        gl::translate(Vec2f(992.5, 285));
+        drawBox();
+        gl::popMatrices();
+        
+        //"面白い"描写
+        gl::pushMatrices();
+        gl::drawString(messageList[3],Vec2f(992.5, 355), mFontColor, mFont);
+        gl::translate(Vec2f(992.5, 355));
+        drawBox();
+        gl::popMatrices();
+        
+        //"トイレにいきたい"描写
+        gl::pushMatrices();
+        gl::drawString(messageList[4],Vec2f(992.5, 425), mFontColor, mFont);
+        gl::translate(Vec2f(992.5, 425));
+        drawBox();
+        gl::popMatrices();
+        
+        //"わかった"描写
+        gl::pushMatrices();
+        gl::drawString(messageList[5],Vec2f(992.5, 495), mFontColor, mFont);
+        gl::translate(Vec2f(992.5, 495));
+        drawBox();
+        gl::popMatrices();
+        
+        //"かっこいい"描写
+        gl::pushMatrices();
+        gl::drawString(messageList[6],Vec2f(992.5, 565), mFontColor, mFont);
+        gl::translate(Vec2f(992.5, 565));
+        drawBox();
+        gl::popMatrices();
+        
+        //"速い!"描写
+        gl::pushMatrices();
+        gl::drawString(messageList[7],Vec2f(992.5, 635), mFontColor, mFont);
+        gl::translate(Vec2f(992.5, 635));
+        drawBox();
+        gl::popMatrices();
+        
+        //"わからない"描写
+        gl::pushMatrices();
+        gl::drawString(messageList[8],Vec2f(992.5, 705), mFontColor, mFont);
+        gl::translate(Vec2f(992.5, 705));
+        drawBox();
+        gl::popMatrices();
+    }
+    
+    //枠としてのBoxを描く
+    void drawBox(){
+        setDiffuseColor( ci::ColorA(0.65, 0.83, 0.58));
+        gl::drawStrokedRoundedRect(Rectf(0,0,270,50), 5);//角の丸い四角
+        setDiffuseColor( ci::ColorA( 0.8, 0.8, 0.8 ) );
+    }
+    
     void drawAccessNumber(){
+        
+        int h = sumHands();
+        int j1 = sumJes1();
+        int j2 = sumJes2();
+        int cmn = countMessageNumber();
+        
         gl::drawString("トータルアクセス数", Vec2d(1200,700));
         gl::drawString(to_string(sumOfFrag()), Vec2d(1200,800));
+        gl::drawString("一番多いメッセージナンバー", Vec2d(1200,100));
+        gl::drawString(to_string(cmn), Vec2d(1200,110));
+        gl::drawString("手の数", Vec2d(1200,150));
+        gl::drawString(to_string(h), Vec2d(1200,160));
+        gl::drawString("サークル数", Vec2d(1200,180));
+        gl::drawString(to_string(j1), Vec2d(1200,200));
+        gl::drawString("タップ数", Vec2d(1200,210));
+        gl::drawString(to_string(j2), Vec2d(1200,230));
     }
     //お絵かき（手の軌跡を描写する）
     void drawPainting(){
@@ -589,94 +697,13 @@ public:
             
         }
     }
-    //枠としてのBoxを描く
-    void drawBox(){
-        // 上面
-        gl::drawLine( Vec3f( mLeft, mTop, mBackSide ),
-                     Vec3f( mRight, mTop, mBackSide ) );
-        
-        gl::drawLine( Vec3f( mRight, mTop, mBackSide ),
-                     Vec3f( mRight, mTop, mFrontSide ) );
-        
-        gl::drawLine( Vec3f( mRight, mTop, mFrontSide ),
-                     Vec3f( mLeft, mTop, mFrontSide ) );
-        
-        gl::drawLine( Vec3f( mLeft, mTop, mFrontSide ),
-                     Vec3f( mLeft, mTop, mBackSide ) );
-        
-//        //2/3面
-//        gl::drawLine( Vec3f( mLeft, mTop*2/3, mBackSide ),
-//                     Vec3f( mRight, mTop*2/3, mBackSide ) );
-//        
-//        gl::drawLine( Vec3f( mRight, mTop*2/3, mBackSide ),
-//                     Vec3f( mRight, mTop*2/3, mFrontSide ) );
-//        
-//        gl::drawLine( Vec3f( mRight, mTop*2/3, mFrontSide ),
-//                     Vec3f( mLeft, mTop*2/3, mFrontSide ) );
-//        
-//        gl::drawLine( Vec3f( mLeft, mTop*2/3, mFrontSide ),
-//                     Vec3f( mLeft, mTop*2/3, mBackSide ) );
-//        
-        // 中面
-        gl::drawLine( Vec3f( mLeft, mTop/2, mBackSide ),
-                     Vec3f( mRight, mTop/2, mBackSide ) );
-        
-        gl::drawLine( Vec3f( mRight, mTop/2, mBackSide ),
-                     Vec3f( mRight, mTop/2, mFrontSide ) );
-        
-        gl::drawLine( Vec3f( mRight, mTop/2, mFrontSide ),
-                     Vec3f( mLeft, mTop/2, mFrontSide ) );
-        
-        gl::drawLine( Vec3f( mLeft, mTop/2, mFrontSide ),
-                     Vec3f( mLeft, mTop/2, mBackSide ) );
-        
-        
-        //中心線
-        gl::drawLine( Vec3f( mLeft, mTop/2, 0 ),
-                     Vec3f( mRight, mTop/2, 0 ) );
-
-        gl::drawLine( Vec3f( 0, mTop/2, mBackSide ),
-                     Vec3f( 0, mTop/2, mFrontSide ) );
-        
-        gl::drawLine( Vec3f( mRight/2, 0, 0 ),
-                     Vec3f( mRight/2, mTop, 0 ) );
-        
-        
-        // 下面
-        gl::drawLine( Vec3f( mLeft, mBottom, mBackSide ),
-                     Vec3f( mRight, mBottom, mBackSide ) );
-        
-        gl::drawLine( Vec3f( mRight, mBottom, mBackSide ),
-                     Vec3f( mRight, mBottom, mFrontSide ) );
-        
-        gl::drawLine( Vec3f( mRight, mBottom, mFrontSide ),
-                     Vec3f( mLeft, mBottom, mFrontSide ) );
-        
-        gl::drawLine( Vec3f( mLeft, mBottom, mFrontSide ),
-                     Vec3f( mLeft, mBottom, mBackSide ) );
-        
-        // 側面
-        gl::drawLine( Vec3f( mLeft, mTop, mFrontSide ),
-                     Vec3f( mLeft, mBottom, mFrontSide ) );
-        
-        gl::drawLine( Vec3f( mLeft, mTop, mBackSide ),
-                     Vec3f( mLeft, mBottom, mBackSide ) );
-        
-        gl::drawLine( Vec3f( mRight, mTop, mFrontSide ),
-                     Vec3f( mRight, mBottom, mFrontSide ) );
-        
-        gl::drawLine( Vec3f( mRight, mTop, mBackSide ),
-                     Vec3f( mRight, mBottom, mBackSide ) );
-    
-    }
-    
-    
     
     //音声解析の描写
     void drawAudioAnalyze(){
         glPushMatrix();
+        glScalef(0.5, 0.5, 0);
         mSpectrumPlot.draw( mMagSpectrum );
-        //drawLabels();
+        drawLabels();
         glPopMatrix();
     }
     
@@ -746,6 +773,7 @@ public:
     
     //フォント
     Font mFont;
+    Color mFontColor;
     
     float mRotateMatrix0;//親指（向かって右足）の回転
     float mRotateMatrix2;//人さし指（向かって右腕）の回転
@@ -802,6 +830,12 @@ public:
     float t1;  //静止画用経過時間（X座標）
     float t2;  //アニメーション用経過時間（X座標）
     float speed = 1.0;    //アニメーションのスピード
+    
+    //周回sinグラフ
+    int posX, posY;//移動する円の位置
+    int R = 5;  //軌跡を描く円の半径（初期値を100）
+    int angle = 0;  //角度
+    
     
     Leap::Controller mLeap;
     Leap::Frame mCurrentFrame;//現在のフレーム
@@ -871,6 +905,8 @@ public:
     
     TriMesh			mFoot;
     gl::VboMesh		vFoot;
+    
+    
 };
 CINDER_APP_NATIVE( LeapApp, RendererGl )
 
@@ -934,7 +970,6 @@ void socketSv(){
     if (l < 0) error("ERROR writing to socket");
     close(newsockfd);
 }
-
 
 void *socketSv_loop(void *p){
     setupSocketSv();
